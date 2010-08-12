4797f154d07bb799b1283c81bee3ec709a96df56
KirbySaysHi <kirbysayshi@gmail.com>
2010-08-12 00:16:56 -0400

    removed constraints between trackers... probably need a separate constraint property for nodes

--- filtration.js
+++ filtration.js
@@ -1,5 +1,130 @@
 (function(){
 
+var vec3 = {
+	ignited: false
+	,setType: function(){
+		if(typeof Float32Array !== 'undefined') { vec3.setType = Float32Array; }
+		else { vec3.setType = Array; }
+		vec3.ignited = true;
+	}
+	,a: function(x, y, z){
+		if(vec3.ignited === false) { vec3.setType(); }
+		var v = new vec3.setType(3);
+		v[0] = x;
+		v[1] = y;
+		v[2] = z;
+		return v;//vec3.a(x, y, z);
+	}
+	,add: function(v1, v2){
+		return vec3.a( v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2] );
+	}
+	,sub: function(v1, v2){
+		return vec3.a( v1[0]-v2[0], v1[1]-v2[1], v1[2]-v2[2] );
+	}
+	,normalize: function(v1){
+		var x = v1[0], y = v1[1], z = v1[2];
+		var len = Math.sqrt(x*x + y*y + z*z);
+
+		if (!len) {
+			return vec3.a(0,0,0);
+		} else if (len == 1) {
+			return vec3.a(x,y,z);
+		}
+
+		len = 1 / len;
+		return vec3.a(x*len, y*len, z*len);
+	}
+	,length: function(v1){
+		var x = v1[0], y = v1[1], z = v1[2];
+		return Math.sqrt(x*x + y*y + z*z);
+	}
+	,dot: function(v1, v2){
+		return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+	}
+	,scale: function(v1, s){
+		return vec3.a(v1[0]*s, v1[1]*s, v1[2]*s);
+	}
+	,clone: function(v1){
+		return vec3.a(v1[0], v1[1], v1[2]);
+	}
+};
+
+// r is a random between 0 and 1
+function Node(ist){
+	var r = Math.random();
+	this.ist = ist;//r > 0.9 ? true : false; // is source of infection
+	this.rad = (50 * r) + 10; // radius/bandwidth/gravity, min of 10
+	this.rad2 = this.rad*this.rad;
+	this.res = this.ist ? 99 : (100 * r) + 10; // resistance to becoming clean, min of 10
+	this.ppos = vec3.a(0,0,0); // previous position
+	this.cpos = vec3.a(0,0,0); // current position
+	this.acl = vec3.a(0,0,0); // acceleration
+	// TODO: make a tracker's mass... massive
+	this.invMass = this.ist ? 1/9000 : 1/this.rad;
+	this.cto = []; // connected to
+	this.dnes = 1; // how dirty the node is (between 0 and 1)
+}
+
+Node.prototype = {
+	BB: function(){
+		return;
+	}
+	,emitPackets: function(){
+		// emit packets based on bandwidth... maybe new var
+	}
+	,aabb: function(){
+		var t = this;
+		var max = vec3.a(-9000,-9000,-9000);
+		var min = vec3.a(9000,9000,9000);
+
+		for(var j = 0; j < t.cto.length; j++){
+			var n = t.cto[j];
+			max[0] = Math.max( max[0], n.cpos[0] );
+			max[1] = Math.max( max[1], n.cpos[1] );
+			max[2] = Math.max( max[2], n.cpos[2] );
+
+			min[0] = Math.min( min[0], n.cpos[0] );
+			min[1] = Math.min( min[1], n.cpos[1] );
+			min[2] = Math.min( min[2], n.cpos[2] );
+
+			// TODO: base this on the cpos + radius, and make it a sphere, not a box
+			// Could we just "know" based on the node with the largest radius?
+		}
+		return {min: min, max: max};
+	}
+	,bsRad: function(){
+		var bnode,
+			brad = 0;
+		for(var j = 0; j < this.cto.length; j++){
+			var n = this.cto[j];
+			if(n.rad > brad && n.ist === false){
+				brad = n.rad;
+				bnode = n;
+			}
+		}
+		var d = vec3.length( vec3.sub(this.cpos, bnode.cpos) );
+		return d + brad;
+	}
+	,findPeers: function(){
+		// shoot lines every n radians
+		// for each line, test for intersecting nodes
+		// only take the closest nodes in the event of two collisions on the same line
+
+	}
+
+};
+
+function Packet(snode, tnode, dnes){
+	this.rad = 2;
+	this.dnes = dnes;
+	this.ppos = [];
+	this.cpos = [];
+	this.snode = snode; // the source node, so it does not go towards source 
+	this.tnode = tnode; // the target node, HOMING MISSILE ACTION!
+}
+
+
+
 function Stage(id){
 	var 
 		 cvs = document.getElementById(id)
@@ -32,9 +157,12 @@ function Stage(id){
 			
 			ctx.fillStyle = frgba;
 			ctx.strokeStyle = "#CCCCCC";
+			ctx.lineWidth = 5;
 			ctx.beginPath();
 			ctx.arc(n.cpos[0], n.cpos[1], n.rad * (1 - (n.cpos[2] / dim[2])), 0, Math.PI*2, false);
+			ctx.stroke();
 			ctx.fill();
+			ctx.lineWidth = 1;
 			
 			if(n.cto.length > 0){
 				ctx.beginPath();
@@ -50,6 +178,12 @@ function Stage(id){
 				}
 				ctx.stroke();
 			}
+			if(n.ist === true){
+				ctx.strokeStyle = "#FFFFFF";
+				ctx.beginPath();
+				ctx.arc(n.cpos[0], n.cpos[1], n.bsRad(), 0, Math.PI*2, false);
+				ctx.stroke();
+			}
 		}
 		
 		ctx.fillStyle = "#FFFFFF";
@@ -63,10 +197,11 @@ function Stage(id){
 			
 			for(var j = 0; j < t.cto.length; j++){
 				var n = t.cto[j];
-				var restLength = n.ist ? (n.res + (n.rad + t.rad)) * 2 : ((n.rad + t.rad)) * 1.5;
+				if(n.ist === true) { continue; }
+				var restLength = n.ist ? (n.res + (n.rad + t.rad)) * 2 : ((n.rad + t.rad)) * 2;
 				var restLength2 = restLength*restLength;
 				var invMass = n.invMass + t.invMass;
-				if( invMass < 0.00001 ) continue;
+				if( invMass < 0.00001 ) { continue; }
 				
 				var delta  = vec3.sub(n.cpos, t.cpos);
 				var delta2 = vec3.dot(delta, delta);
@@ -87,13 +222,13 @@ function Stage(id){
 		
 			for(var j = 0; j < nlist.length; j++){
 				var n2 = nlist[j];
-				if(n1 == n2) continue;
+				if(n1 == n2) { continue; }
 				var colVec =  vec3.sub(n1.cpos, n2.cpos); //n1.getCollisionDepth(n2)
 				var combinedRadius = n1.rad + n2.rad;
 				var dotCol = vec3.dot(colVec, colVec); // effectively the distance squared
 				var radius2 = combinedRadius*combinedRadius;
 				
-				if(dotCol > radius2){
+				if(dotCol >= radius2){
 					continue;
 				}
 			
@@ -116,7 +251,7 @@ function Stage(id){
 				n1.cpos = vec3.add(n1.cpos,
 					vec3.scale(colVec, distance * n1.invMass));
 				
-				// I'm not sure if this actually works, but it seems like it
+				//I'm not sure if this actually works, but it seems like it
 				n2.ppos = vec3.sub(n2.ppos, 
 					vec3.scale(colVec, distance * n2.invMass * (friction)));
 				n1.ppos = vec3.add(n1.ppos,                     
@@ -144,13 +279,13 @@ function Stage(id){
 	}
 	
 	function checkBounds(node){
-		node.cpos[0] = Math.max( 0 + node.rad, Math.min(node.cpos[0], dim[0] - node.rad) );
-		node.cpos[1] = Math.max( 0 + node.rad, Math.min(node.cpos[1], dim[1] - node.rad) );
-		node.cpos[2] = Math.max( 0 + node.rad, Math.min(node.cpos[2], dim[2] - node.rad) );
-		
-		node.ppos[0] = Math.max( 0 + node.rad, Math.min(node.ppos[0], dim[0] - node.rad) );
-		node.ppos[1] = Math.max( 0 + node.rad, Math.min(node.ppos[1], dim[1] - node.rad) );
-		node.ppos[2] = Math.max( 0 + node.rad, Math.min(node.ppos[2], dim[2] - node.rad) );
+		//node.cpos[0] = Math.max( 0 + node.rad, Math.min(node.cpos[0], dim[0] - node.rad) );
+		//node.cpos[1] = Math.max( 0 + node.rad, Math.min(node.cpos[1], dim[1] - node.rad) );
+		//node.cpos[2] = Math.max( 0 + node.rad, Math.min(node.cpos[2], dim[2] - node.rad) );
+		//
+		//node.ppos[0] = Math.max( 0 + node.rad, Math.min(node.ppos[0], dim[0] - node.rad) );
+		//node.ppos[1] = Math.max( 0 + node.rad, Math.min(node.ppos[1], dim[1] - node.rad) );
+		//node.ppos[2] = Math.max( 0 + node.rad, Math.min(node.ppos[2], dim[2] - node.rad) );
 		
 		node.cpos[2] = 0; // uncomment this to disable 3D
 		node.ppos[2] = 0; // uncomment this to disable 3D
@@ -159,6 +294,7 @@ function Stage(id){
 	function goVerlet(dt){
 		for(var i = 0; i < nlist.length; i++){
 			var n1 = nlist[i];
+			if(n1.ist) { continue; } // trackers are fixed...
 		
 			// add gravity temporarily
 			n1.acl = vec3.add(n1.acl, vec3.a(0, 0, -100));
@@ -216,17 +352,33 @@ function Stage(id){
 		}
 		
 		// connect the trackers in series
-		if(i != 0){
+		if(i !== 0){
 			t.cto.push( tlist[i-1] );
 		}
 	}
 	
-	// make a few node-to-node connections
-	for(var i = tMax; i < nlist.length; i += tMax){
-		var n = nlist[i];
-		if(n.ist == true) continue;
-		n.cto.push(nlist[i-tMax]);
+	// run this so that bounding spheres are accurate
+	resolveConstraints();
+	
+	// update placement of trackers based on their nodes so all are onscreen
+	for (var i = 0; i < tlist.length; i++) {
+		var t = tlist[i];
+		var r = t.bsRad();
+		t.cpos[0] = Math.max( 0 + r, Math.min(t.cpos[0], dim[0] - r) );
+		t.cpos[1] = Math.max( 0 + r, Math.min(t.cpos[1], dim[1] - r) );
+		//t.cpos[2] = Math.max( 0 + r, Math.min(t.cpos[2], dim[2] - r) );
+		                                                          
+		t.ppos[0] = Math.max( 0 + r, Math.min(t.ppos[0], dim[0] - r) );
+		t.ppos[1] = Math.max( 0 + r, Math.min(t.ppos[1], dim[1] - r) );
+		//t.ppos[2] = Math.max( 0 + r, Math.min(t.ppos[2], dim[2] - r) );
 	}
+	
+	// make a few node-to-node connections
+	//for(var i = tMax; i < nlist.length; i += tMax){
+	//	var n = nlist[i];
+	//	if(n.ist == true) continue;
+	//	n.cto.push(nlist[i-tMax]);
+	//}
 	
 	var grabbed = false;
 	
@@ -248,7 +400,7 @@ function Stage(id){
 	}, false);
 	
 	cvs.addEventListener("mousemove", function(e){
-		if(grabbed != false){
+		if(grabbed !== false){
 			grabbed.cpos[0] = e.clientX;
 			grabbed.cpos[1] = e.clientY;
 			grabbed.cpos[2] = 0;
@@ -266,90 +418,7 @@ function Stage(id){
 		, goVerlet: goVerlet
 		, resolveNodeCollisions: resolveNodeCollisions 
 		, resolveConstraints: resolveConstraints
-	}
-}
-
-// r is a random between 0 and 1
-function Node(ist){
-	var r = Math.random();
-	this.ist = ist;//r > 0.9 ? true : false; // is source of infection
-	this.rad = (50 * r) + 10; // radius/bandwidth/gravity, min of 10
-	this.rad2 = this.rad*this.rad;
-	this.res = this.ist ? 99 : (100 * r) + 10; // resistance to becoming clean, min of 10
-	this.ppos = vec3.a(0,0,0); // previous position
-	this.cpos = vec3.a(0,0,0); // current position
-	this.acl = vec3.a(0,0,0); // acceleration
-	this.invMass = 1/this.rad;
-	this.cto = []; // connected to
-	this.dnes = 1; // how dirty the node is (between 0 and 1)
-}
-
-Node.prototype = {
-	BB: function(){
-		return;
-	}
-	,emitPackets: function(){
-		// emit packets based on bandwidth... maybe new var
-	}
-
-};
-
-function Packet(snode, tnode, dnes){
-	this.rad = 2;
-	this.dnes = dnes;
-	this.ppos = [];
-	this.cpos = [];
-	this.snode = snode; // the source node, so it does not go towards source 
-	this.tnode = tnode; // the target node, HOMING MISSILE ACTION!
-}
-
-var vec3 = {
-	ignited: false
-	,setType: function(){
-		if(typeof Float32Array !== 'undefined') vec3.setType = Float32Array;
-		else vec3.setType = Array;
-		vec3.ignited = true;
-	}
-	,a: function(x, y, z){
-		if(vec3.ignited == false) vec3.setType();
-		var v = new vec3.setType(3);
-		v[0] = x;
-		v[1] = y;
-		v[2] = z;
-		return v;//vec3.a(x, y, z);
-	}
-	,add: function(v1, v2){
-		return vec3.a( v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2] );
-	}
-	,sub: function(v1, v2){
-		return vec3.a( v1[0]-v2[0], v1[1]-v2[1], v1[2]-v2[2] );
-	}
-	,normalize: function(v1){
-		var x = v1[0], y = v1[1], z = v1[2];
-		var len = Math.sqrt(x*x + y*y + z*z);
-
-		if (!len) {
-			return vec3.a(0,0,0);
-		} else if (len == 1) {
-			return vec3.a(x,y,z);
-		}
-
-		len = 1 / len;
-		return vec3.a(x*len, y*len, z*len);
-	}
-	,length: function(v1){
-		var x = v1[0], y = v1[1], z = v1[2];
-		return Math.sqrt(x*x + y*y + z*z);
-	}
-	,dot: function(v1, v2){
-		return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
-	}
-	,scale: function(v1, s){
-		return vec3.a(v1[0]*s, v1[1]*s, v1[2]*s);
-	}
-	,clone: function(v1){
-		return vec3.a(v1[0], v1[1], v1[2]);
-	}
+	};
 }
 
 var S = new Stage("stage");
@@ -368,4 +437,4 @@ document.addEventListener("keydown", function(e){
 	} 
 }, false);
 
-})()
+})();
